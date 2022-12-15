03ffc1c174d33d40d918be74b317cec490a2c8cb
jgarciat1 <115965699+jgarciat1@users.noreply.github.com>
2022-12-14 22:44:33 -0500

    Add files via upload

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -49,6 +49,17 @@ CREATE TABLE "contacts" (
      )
 );
 
+CREATE TABLE "backers" (
+    "backer_id" varchar(10)   NOT NULL,
+    "cf_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_backers" PRIMARY KEY (
+        "backer_id"
+     )
+);
+
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
 REFERENCES "contacts" ("contact_id");
 
@@ -58,3 +69,49 @@ REFERENCES "category" ("category_id");
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
 
+ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "campaign" ("cf_id");
+
+
+-- DBD
+campaign
+-
+cd_id int PK
+contact_id int FK - contacts.contact_id
+company_name varchar(100)
+description text 
+goal numeric(10,2)
+pledged humeric(10,2)
+outcome varchar(50)
+backers_count int
+country varchar(10)
+currency varchar(10)
+launched_date date
+end_date date
+category_id varchar(10) FK >- categories.category_id
+subcategory_id varchar(10) FK >- subcategories.subcategory_id
+
+categories
+-
+category_id varchar(10) pk
+category varchar(50)
+
+subcategories
+-
+subcategory_id varchar(10) pk
+subcategory varchar(50)
+
+contacts
+-
+contact_id int pk
+first_name varchar(50)
+last_name varchar(50)
+email varchar(100)
+
+backers
+-
+backer_id varchar(10) pk
+cf_id int FK >- campaign.cf_id
+first_name varchar(50)
+last_name varchar(50)
+email varchar(100)
